7b08ebf6cd9bb591f4f0e778aa8f2eb0140848e1
Stuffing <stuffingeu56@gmail.com>
2020-11-08 15:08:53 +0300

    Flask резерв

--- flsite.py
+++ flsite.py
@@ -1,28 +1,78 @@
-from flask import Flask, render_template, url_for
+import sqlite3
+import os
+from  flask import Flask, render_template, request, g, flash, abort
+from FDataBase import FDataBase
+
+# конфигурация
+DATABASE = '/tmp/flsite.db'
+DEBUG = True
+SECRET_KEY = 'dasfasf53@#5?.dasfg<ewqk'
 
 app = Flask(__name__)
+app.config.from_object(__name__)
 
-menu = [{"name": "Установка", "url": "install-flask"},
-        {"name": "Первое приложение", "url": "first-app"},
-        {"name": "Обратная связь", "url": "contact"}]
+app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsite.db')))
 
+def connect_db():
+    conn = sqlite3.connect(app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def create_db():
+    """ Вспомогательная функция для создания таблицы БД"""
+    db = connect_db()
+    with app.open_resource('sq_db.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
+
+def get_db():
+    """Соединение с БД, если оно еще не установленно"""
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return g.link_db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """ЗАкрываем соединение с БД, если оно было установленно"""
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
 
 @app.route("/")
 def index():
-    print(url_for('index'))
-    return render_template('index.html', menu=menu)
+    db = get_db()
+    dbase = FDataBase(db)
+    return render_template('index.html', menu=dbase.getMenu(), posts=dbase.getPostsAnonce())
 
 
-@app.route("/about")
-def about():
-    print(url_for('about'))
-    return render_template('about.html', title="О сайте", menu=menu)
+@app.route('/add_post', methods=["POST", "GET"])
+def addPost():
+    db = get_db()
+    dbase = FDataBase(db)
+
+    if request.method == "POST":
+        if len(request.form['name']) > 4 and len(request.form['post']) > 10:
+            res = dbase.addPost(request.form['name'], request.form['post'])
+            if not res:
+                flash('Ошибка добавления статьи', category='error')
+            else:
+                flash('Статья добавлена успешно', category='success')
+        else:
+            flash('Ошибка добавления статьи', category='error')
+
+    return render_template('add_post.html', menu=dbase.getMenu(), title="Добавление статьи")
 
 
-@app.route("/profile/<username>")
-def profile(username):
-    return f"Пользователь: {username}"
+@app.route('/post/<int:id_post>')
+def showPost(id_post):
+    db = get_db()
+    dbase = FDataBase(db)
+    title, post = dbase.getPost(id_post)
+    if not title:
+        abort(404)
 
+    return render_template('post.html', menu=dbase.getMenu(), title=title, post=post)
 
 if __name__ == "__main__":
     app.run(debug=True)
