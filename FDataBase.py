ef1e82856b2d2ed7adf9fb87c540f93309ea3106
Stuffing <stuffingeu56@gmail.com>
2020-11-09 22:09:24 +0300

    reserv

--- FDataBase.py
+++ FDataBase.py
@@ -1,6 +1,8 @@
 import math
 import time
 import sqlite3
+import re
+from flask import url_for
 
 
 class FDataBase:
@@ -19,10 +21,22 @@ class FDataBase:
             print("Ошибка чтения из БД")
         return []
 
-    def addPost(self, title, text):
+    def addPost(self, title, text, url):
         try:
+            self.__cur.execute(f"SELECT COUNT() as 'count'  FROM posts WHERE url LIKE '{url}'")
+            res = self.__cur.fetchone()
+            if res['count'] > 0:
+                print("Статья с таким url уже существует")
+                return False
+
+            base = url_for('static', filename='images_html')
+
+            text = re.sub(r"(?P<tag><img\s+[^>]*src=)(?P<quote>[\"'])(?P<url>.+?)(?P=quote)>",
+                          "\\g<tag>" + base + "/\\g<url>>",
+                          text)
+
             tm = math.floor(time.time())
-            self.__cur.execute("INSERT INTO posts VALUES(NULL, ?, ?, ?)", (title, text, tm))
+            self.__cur.execute("INSERT INTO posts VALUES(NULL, ?, ?, ?, ?)", (title, text, url, tm))
             self.__db.commit()
         except sqlite3.Error as e:
             print("Ошибка добавления статьи в БД" + str(e))
@@ -30,9 +44,9 @@ class FDataBase:
 
         return True
 
-    def getPost(self, postID):
+    def getPost(self, alias):
         try:
-            self.__cur.execute(f"SELECT title, text From posts WHERE id = {postID}")
+            self.__cur.execute(f"SELECT title, text From posts WHERE url LIKE '{alias}'")
             res = self.__cur.fetchone()
             if res:
                 return res
@@ -43,10 +57,55 @@ class FDataBase:
 
     def getPostsAnonce(self):
         try:
-            self.__cur.execute(f"SELECT id, title, text FROM posts ORDER BY time DESC")
+            self.__cur.execute(f"SELECT id, title, text, url FROM posts ORDER BY time DESC")
             res = self.__cur.fetchall()
             if res: return res
         except sqlite3.Error as e:
             print("Ошибка получения статьи из БД" + str(e))
 
         return []
+
+    def addUser(self, name, email, hpsw):
+        try:
+            self.__cur.execute(f"SELECT COUNT() as 'count' FROM users WHERE email LIKE '{email}'")
+            res = self.__cur.fetchone()
+            if res['count'] > 0:
+                print("Пользователь с таким email уже существует")
+                return False
+
+            tm = math.floor(time.time())
+            self.__cur.execute("INSERT INTO users VALUES(NULL, ?, ?, ?, ?)", (name, email, hpsw, tm))
+            self.__db.commit()
+        except sqlite3.Error as e:
+            print("Ошибка добавления пользователей в БД" + str(e))
+            return False
+
+        return True
+
+    def getUser(self, user_id):
+        try:
+            self.__cur.execute(f"SELECT * FROM users WHERE id = {user_id} LIMIT 1")
+            res = self.__cur.fetchone()
+            if not res:
+                print("Пользователь не найден")
+                return False
+
+            return res
+        except sqlite3.Error as e:
+            print("Ошибка получения данных из БД" + str(e))
+
+        return False
+
+    def getUserByEmail(self, email):
+        try:
+            self.__cur.execute(f"SELECT * from users WHERE  email = '{email}' LIMIT 1")
+            res = self.__cur.fetchone()
+            if not res:
+                print("Пользователь не найден")
+                return False
+
+            return res
+        except sqlite3.Error as e:
+            print("Ошибка получения данных из БД" + str(e))
+
+        return False
